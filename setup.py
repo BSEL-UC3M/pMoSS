2d571fd86475c67fe89fb8b03bf4b1150dc59d66
esgomezm <43643518+esgomezm@users.noreply.github.com>
2022-12-21 12:27:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pmoss',
     packages=find_packages(include=['pmoss', 'pmoss.*']),
-    version='1.1',
+    version='2.0',
     license='BSD 3-Clause License',
     description='Python package to model the p-value as an n-dependent function using Monte Carlo cross-validation.',
     long_description=long_description,
@@ -15,7 +15,7 @@ setup(
     author="E. Gomez-de-Mariscal, V. Guerrero, A. Sneider, H. Hayatilaka, J.M. Phillip, D. Wirtz, A. Munoz-Barrutia",
     author_email='egomez@igc.gulbenkian.pt, mamunozb@ing.uc3m.es',
     url='https://github.com/BIIG-UC3M/pMoSS',
-    download_url='https://github.com/BIIG-UC3M/pMoSS/archive/refs/tags/v1.1.tar.gz',
+    download_url='https://github.com/BIIG-UC3M/pMoSS/archive/refs/tags/v2.0.tar.gz',
     keywords=['p-value', 'monte-carlo', 'statistical significance', 'null hypothesis testing', 'statistical test'],
     python_requires='>=3.6',
     install_requires=[
