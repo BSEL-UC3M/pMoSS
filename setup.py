3d3bf99e008551bc5fb2cdc812198426e3c6cef0
esgomezm <egomez@igc.gulbenkian.pt>
2024-03-12 11:20:42 +0200

    freeze dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pmoss',
     packages=find_packages(include=['pmoss', 'pmoss.*']),
-    version='2.0',
+    version='2.1',
     license='BSD 3-Clause License',
     description='Python package to model the p-value as an n-dependent function using Monte Carlo cross-validation.',
     long_description=long_description,
@@ -17,18 +17,18 @@ setup(
     url='https://github.com/BIIG-UC3M/pMoSS',
     download_url='https://github.com/BIIG-UC3M/pMoSS/archive/refs/tags/v2.0.tar.gz',
     keywords=['p-value', 'monte-carlo', 'statistical significance', 'null hypothesis testing', 'statistical test'],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     install_requires=[
-        'numpy',
-        'pandas',
-        'scipy>=1.1.0',
-        'xlrd>=1.0.0',
-        'matplotlib',
-        'seaborn',
-        'statsmodels>=0.9.0',
-        'glob2',
-        'pytest-shutil',
-        'openpyxl'
+        'numpy==1.25.2',
+        'pandas==1.5.3',
+        'scipy==1.12.0',
+        'xlrd==2.0.1',
+        'matplotlib==3.8.3',
+        'seaborn==0.13.2',
+        'statsmodels==0.14.1',
+        'glob2==0.7',
+        'pytest-shutil==1.7.0',
+        'openpyxl==3.1.2'
       ],
     classifiers=[
         'Development Status :: 3 - Alpha',
